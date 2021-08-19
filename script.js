77f280ab556646841919e35fdf612945b60efec3
Jesus <jmen95@misena.edu.co>
2021-08-19 08:52:44 -0500

    :heavy_plus_sign: ADD: Create message card

--- script.js
+++ script.js
@@ -25,6 +25,21 @@ async function getDataZippopotomus(url) {
   return json;
 }
 
-getDataZippopotomus("90210").then((response) => {
-  console.log(`response`, response);
-});
+const $message = document.querySelector(".message");
+//Type 1= Success, 2= error
+function message(text, type = 1) {
+  const messageType = type === 2 ? "error" : "success";
+  $message.innerHTML = text;
+  $message.classList.remove("show");
+  $message.classList.remove("message--success");
+  $message.classList.remove("message--error");
+  $message.classList.add("show");
+  $message.classList.add("message--" + messageType);
+}
+
+function hideMessage() {
+  $message.innerHTML = "";
+  $message.classList.remove("show");
+  $message.classList.remove("message--success");
+  $message.classList.remove("message--error");
+}
