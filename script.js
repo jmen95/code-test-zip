2bb7c5df274e732d22e4aec579e55f3a6284a214
Jesus <jmen95@misena.edu.co>
2021-08-19 09:10:36 -0500

    :heavy_plus_sign: ADD: get API Data and add data to de DOM

--- script.js
+++ script.js
@@ -43,3 +43,112 @@ function hideMessage() {
   $message.classList.remove("message--success");
   $message.classList.remove("message--error");
 }
+
+const $resultSearch = document.querySelector("#resultSearch");
+
+/**
+ * Get info item HTML
+ * @param {string} title
+ * @param {string} description
+ * @returns HTML info item
+ */
+function infoItemHTML(title, description) {
+  return `<li class="info__item">
+    <strong>${title}:</strong> ${description}
+</li>`;
+}
+
+/**
+ * Place card info
+ * @param {*} place
+ * @returns
+ */
+function infoPlaceCard(place) {
+  return `<div class="place">
+<div class="place__aside">
+    <img src="states/${place["state abbreviation"]}.svg" alt="${
+    place["state"]
+  }" class="place__aside__img" width="200" />
+</div>
+<h4 class="place-title">${place["place name"]}</h4>
+<ul class="info">
+    ${infoItemHTML("State", place["state"])}
+    ${infoItemHTML("State Abbreviation", place["state abbreviation"])}
+    ${infoItemHTML("Latitude", place["latitude"])}
+    ${infoItemHTML("Longitude", place["longitude"])}
+  
+</ul>
+</div>`;
+}
+
+/**
+ * Print zipcode data
+ * @param {*} zipCodeData
+ */
+function printSearchResult(zipCodeData) {
+  const HTML = `<section class="details">
+  <h2 class="subtitle">Post Code: ${zipCodeData["post code"]}</h2>
+  <ul class="info">
+      ${infoItemHTML("Country", zipCodeData["country"])}
+      ${infoItemHTML(
+        "Country Abbreviation",
+        zipCodeData["country abbreviation"]
+      )}
+  </ul>
+  <h3 class="places-title">Places</h3>
+  ${zipCodeData.places?.map((place) => infoPlaceCard(place))}
+</section>`;
+  $resultSearch.innerHTML = HTML;
+}
+
+function noResultsFound() {
+  message("No zip code found.", 2);
+  $resultSearch.innerHTML = "";
+}
+
+/**
+ * Events
+ */
+
+const $form = document.querySelector("#frmSearch");
+const $zipCode = document.querySelector("#zipCode");
+/**
+ * Handle submit form
+ * @param {*} event
+ */
+async function handleSubmit(event) {
+  event.preventDefault();
+  hideMessage();
+  $form.classList.remove("validated");
+  if ($form.checkValidity()) {
+    const zipCode = $zipCode.value;
+    if (/^[\d]{5}$/.test(zipCode)) {
+      try {
+        const zipCodeData = await getDataZippopotomus(zipCode);
+        if (Object.keys(zipCodeData).length) {
+          printSearchResult(zipCodeData);
+        } else {
+          noResultsFound();
+        }
+      } catch (error) {
+        console.error(`handleSubmit Error`, error);
+        message("Error to connect with API", 2);
+      }
+    } else {
+      $form.classList.add("validated");
+      $zipCode.setCustomValidity("Invalid zip code.");
+      message("Invalid zip code.", 2);
+    }
+  } else {
+    $form.classList.add("validated");
+  }
+}
+function handleChange() {
+  if (this.value) {
+    this.setCustomValidity("");
+  }
+}
+
+$form.addEventListener("submit", handleSubmit);
+
+$zipCode.addEventListener("change", handleChange);
